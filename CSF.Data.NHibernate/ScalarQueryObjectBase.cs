6e875b570d9a896aad22fecbb0eaf665923bb47a
Craig Fowler <craig@csf-dev.com>
2017-03-01 20:45:11 +0000

    Add base types for Enhanced Query Objects pattern

---
+++ CSF.Data.NHibernate/ScalarQueryObjectBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NHibernate;
+
+namespace CSF.Data.NHibernate
+{
+  public abstract class ScalarQueryObjectBase<T>
+  {
+    readonly ISession session;
+
+    protected virtual ISession Session => session;
+
+    public abstract T GetResult();
+
+    public ScalarQueryObjectBase(ISession session)
+    {
+      if(session == null)
+        throw new ArgumentNullException(nameof(session));
+
+      this.session = session;
+    }
+  }
+}
